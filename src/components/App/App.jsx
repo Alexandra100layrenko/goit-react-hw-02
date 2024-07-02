69fd26b6e35c947bf9b3c38a31d45568a934678e
Alexandra100layrenko <66309809+Alexandra100layrenko@users.noreply.github.com>
2024-07-02 19:32:22 +0100

    update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect  } from 'react';
+import { useState, useEffect  } from 'react';
 import Feedback from '../Feedback/Feedback';
 import Options from '../Options/Options';
 import Notification from '../Notification/Notification'
