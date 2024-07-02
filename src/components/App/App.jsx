17e68dc394dc58df2ba8cd2cedb660e62771c01d
Alexandra100layrenko <66309809+Alexandra100layrenko@users.noreply.github.com>
2024-07-02 18:44:09 +0100

    update all

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Feedback from '../Feedback/Feedback';
 import Options from '../Options/Options';
+import Notification from '../Notification/Notification'
 import styles from './App.module.css'
 
 export default function App() {
@@ -14,12 +15,22 @@ export default function App() {
     }));
   };
 
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  
+  const resetClicks = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  }
+
   return (
     <div className={styles.container}>
       <h1>Sip Happens Café</h1>
       <p>Please leave your feedback about our service by selecting one of the options below.</p>
-      <Options onFeedback={handleFeedback} />
-      <Feedback feedback={feedback} />
+      <Options onFeedback={handleFeedback} totalFeedback={totalFeedback} onReset={resetClicks} />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} />
+      ) : (
+        <Notification message="No feedback given yet" />
+      )}
     </div>
   );
 }
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,12 +1,12 @@
 import styles from './Options.module.css';
 
-export default function Options({ onFeedback }) {
+export default function Options({ onFeedback, totalFeedback, onReset }) {
   return (
     <div className={styles.options}>
       <button onClick={() => onFeedback('good')}>Good</button>
       <button onClick={() => onFeedback('neutral')}>Neutral</button>
       <button onClick={() => onFeedback('bad')}>Bad</button>
-      <button onClick={() => onFeedback('reset')}>Reset</button>
+      {totalFeedback > 0 && <button onClick={onReset}>Reset</button>}
     </div>
   );
 }
